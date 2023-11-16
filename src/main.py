c4d37aef7b323ad93da907d5cd7ba98f0273bfc7
Melyandr <119205312+Melyandr@users.noreply.github.com>
2023-11-16 20:24:34 +0200

    added fifth lab with considering more than 2 tribes

--- src/main.py
+++ src/main.py
@@ -1,58 +1,56 @@
 def couples_for_wedding(graph):
-    first_tribe = set()
-    second_tribe = set()
-    for person in graph[1]:
-        first_tribe.add(person)
+    if len(graph) == 0 | len(graph) == 1:
+        return set()
 
-    for person_first_tribe in graph[1]:
-        for sublist in graph[2:]:
-            if person_first_tribe in sublist:
-                for new_person in sublist:
-                    if new_person not in graph[1]:
-                        first_tribe.add(new_person)
-                        graph[1].append(new_person)
-                        sublist_for_removing = sublist
-                graph.remove(sublist_for_removing)
-
-    second_tribe.update(graph[2])
-
-    odd_numbers = set()
-    paired_numbers = set()
-
-    for person in graph[1]:
-        if person % 2 == 0:
-            paired_numbers.add(person)
-        else:
-            odd_numbers.add(person)
+    tribes = [set() for _ in range(len(graph))]
+    i = 1
+    while i < len(graph):
+        for person in graph[i]:
+            tribes[i].add(person)
+            j = i + 1
+            while j < len(graph):
+                if person in graph[j]:
+                    for new_person in graph[j]:
+                        if new_person not in graph[i]:
+                            tribes[i].add(new_person)
+                            graph[i].append(new_person)
+                    graph.remove(graph[j])
+                else:
+                    j += 1
+        i += 1
 
     set_output = set()
 
-    for boy_first_tribe in odd_numbers:
-        for person_second_tribe in graph[2]:
-            if (boy_first_tribe + person_second_tribe) % 2 != 0:
-                set_output.add(f"{boy_first_tribe}/" f"{person_second_tribe}")
-
-    for girl_first_tribe in paired_numbers:
-        for person_second_tribe in graph[2]:
-            if (girl_first_tribe + person_second_tribe) % 2 != 0:
-                set_output.add(f"{girl_first_tribe}/" f"{person_second_tribe}")
+    for i in range(len(tribes)):
+        odd_numbers = {person for person in tribes[i] if person % 2 != 0}
+        paired_numbers = {person for person in tribes[i] if person % 2 == 0}
+        for j in range(i + 1, len(tribes)):
+            for boy in odd_numbers:
+                for person in tribes[j]:
+                    if (boy + person) % 2 != 0:
+                        set_output.add(f"{boy}/{person}")
+            for girl in paired_numbers:
+                for person in tribes[j]:
+                    if (girl + person) % 2 != 0:
+                        set_output.add(f"{girl}/{person}")
 
     return set_output
 
 
 def read_from_file():
-    with open("../tests/input.txt", "r") as input_file:
+    with open("../test/input2.txt", "r") as input_file:
         lines = input_file.readlines()
 
     graph = []
     for line in lines:
-        part = list(map(int, line.split(" ")))
-        graph.append(part)
+        if line.strip():
+            part = list(map(int, line.split(" ")))
+            graph.append(part)
     return graph
 
 
 def write_to_file(couples, count):
-    with open("../tests/output.txt", "w") as output_file:
+    with open("../test/output.txt", "w") as output_file:
         if couples:
             output_file.write(f"Можливі комбінації пар: {count} (")
             output_file.write((", ".join(couples)))
@@ -64,6 +62,9 @@ def write_to_file(couples, count):
 
 if __name__ == "__main__":
     graph = read_from_file()
+
     combinations = couples_for_wedding(graph)
     count_couples = len(combinations)
+    print(count_couples)
+    print(combinations)
     write_to_file(combinations, count_couples)
--- test/test.py
+++ test/test.py
@@ -3,6 +3,8 @@ from src.main import couples_for_wedding
 
 graph1 = [[3], [1, 2], [2, 4], [3, 5]]
 graph2 = [[5], [1, 2], [2, 4], [1, 3], [3, 5], [8, 10]]
+empty_graph = []
+one_couple_graph = [[1, 3]]
 
 
 class MyTestCase(unittest.TestCase):
@@ -13,6 +15,12 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(
             couples_for_wedding(graph2), {"1/10", "5/10", "1/8", "3/8", "5/8", "3/10"}
         )
+
+    def test_empty_graph(self):
+        self.assertEqual(couples_for_wedding(empty_graph), set())
+
+    def test_one_couple(self):
+        self.assertEqual(couples_for_wedding(one_couple_graph), set())
 
 
 if __name__ == "__main__":
