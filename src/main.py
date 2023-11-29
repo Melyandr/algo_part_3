a540749bd5386549c0438eb233b1b60933622670
Melyandr <119205312+Melyandr@users.noreply.github.com>
2023-11-29 14:58:13 +0200

    added  6laba

--- src/main.py
+++ src/main.py
@@ -1,70 +1,60 @@
-def couples_for_wedding(graph):
-    if len(graph) == 0 | len(graph) == 1:
-        return set()
-
-    tribes = [set() for _ in range(len(graph))]
-    i = 1
-    while i < len(graph):
-        for person in graph[i]:
-            tribes[i].add(person)
-            j = i + 1
-            while j < len(graph):
-                if person in graph[j]:
-                    for new_person in graph[j]:
-                        if new_person not in graph[i]:
-                            tribes[i].add(new_person)
-                            graph[i].append(new_person)
-                    graph.remove(graph[j])
-                else:
-                    j += 1
-        i += 1
-
-    set_output = set()
-
-    for i in range(len(tribes)):
-        odd_numbers = {person for person in tribes[i] if person % 2 != 0}
-        paired_numbers = {person for person in tribes[i] if person % 2 == 0}
-        for j in range(i + 1, len(tribes)):
-            for boy in odd_numbers:
-                for person in tribes[j]:
-                    if (boy + person) % 2 != 0:
-                        set_output.add(f"{boy}/{person}")
-            for girl in paired_numbers:
-                for person in tribes[j]:
-                    if (girl + person) % 2 != 0:
-                        set_output.add(f"{girl}/{person}")
-
-    return set_output
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.end_of_word = False
 
 
-def read_from_file():
-    with open("../test/input2.txt", "r") as input_file:
-        lines = input_file.readlines()
+class PrefixTree:
+    def __init__(self):
+        self.root = Node()
 
-    graph = []
-    for line in lines:
-        if line.strip():
-            part = list(map(int, line.split(" ")))
-            graph.append(part)
-    return graph
+    def insert(self, word: str):
+        pointer = self.root
+        for letter in word:
+            if letter not in pointer.children:
+                pointer.children[letter] = Node()
+            pointer = pointer.children[letter]
+        pointer.end_of_word = True
+
+    def search(self, word: str) -> bool:
+        pointer = self.root
+        for letter in word:
+            if letter not in pointer.children:
+                return False
+            pointer = pointer.children[letter]
+
+        if pointer.end_of_word:
+            return True
+        else:
+            return False
+
+    def starts_with(self, prefix: str) -> bool:
+        pointer = self.root
+        for letter in prefix:
+            if letter not in pointer.children:
+                return False
+            pointer = pointer.children[letter]
+        return True
 
 
-def write_to_file(couples, count):
-    with open("../test/output.txt", "w") as output_file:
-        if couples:
-            output_file.write(f"Можливі комбінації пар: {count} (")
-            output_file.write((", ".join(couples)))
-
-        output_file.write(")")
-
-        output_file.close()
+def fill_words_to_trie(patterns) -> PrefixTree:
+    obj = PrefixTree()
+    for i in patterns:
+        obj.insert(i)
+    return obj
 
 
 if __name__ == "__main__":
-    graph = read_from_file()
+    # obj = PrefixTree()
+    # obj.insert("dog")
+    # obj.insert("dos")
+    # obj.insert("do")
+    # obj.insert("mul")
+    # print(obj.starts_with("do"))
+    # print(obj.search("dos"))
+    patterns = ["do", "dos", "dog", "mul"]
+    obj_with_patterns = fill_words_to_trie(patterns)
+    print(obj_with_patterns.search("mul"))
+    print(obj_with_patterns.starts_with("p"))
 
-    combinations = couples_for_wedding(graph)
-    count_couples = len(combinations)
-    print(count_couples)
-    print(combinations)
-    write_to_file(combinations, count_couples)
+    # print(obj_with_patterns)
--- test/test.py
+++ test/test.py
@@ -1,26 +1,20 @@
 import unittest
-from src.main import couples_for_wedding
-
-graph1 = [[3], [1, 2], [2, 4], [3, 5]]
-graph2 = [[5], [1, 2], [2, 4], [1, 3], [3, 5], [8, 10]]
-empty_graph = []
-one_couple_graph = [[1, 3]]
+from src.main import PrefixTree
 
 
-class MyTestCase(unittest.TestCase):
-    def test_graph1(self):
-        self.assertEqual(couples_for_wedding(graph1), {"2/5", "4/3", "2/3", "4/5"})
+class TestPrefixTree(unittest.TestCase):
+    def setUp(self):
+        self.obj = PrefixTree()
+        self.obj.insert("dog")
+        self.obj.insert("dos")
+        self.obj.insert("do")
+        self.obj.insert("mul")
 
-    def test_graph2(self):
-        self.assertEqual(
-            couples_for_wedding(graph2), {"1/10", "5/10", "1/8", "3/8", "5/8", "3/10"}
-        )
+    def test_search(self):
+        self.assertEqual(self.obj.search("dos"), True)
 
-    def test_empty_graph(self):
-        self.assertEqual(couples_for_wedding(empty_graph), set())
-
-    def test_one_couple(self):
-        self.assertEqual(couples_for_wedding(one_couple_graph), set())
+    def test_start_with(self):
+        self.assertEqual(self.obj.starts_with("d"), True)
 
 
 if __name__ == "__main__":
